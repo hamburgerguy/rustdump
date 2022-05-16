18011c32627dcde3c3b7edfa2b64678f6e29232c
Acorbo <adamryancorbo@gmail.com>
2022-05-16 12:29:55 -0400

    suck

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::collections::HashMap;
+use rand::Rng;
 
 fn main() {
-    let mut map = HashMap::new();
-    map.insert(1,2);
-
-    println!("{}", map);
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The random number is: {}", secret_number);
 }
